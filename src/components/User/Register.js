cfd475b14f37a175cc63e6b1f7133aa9e73d0df4
A-dith-ya <awedithya@gmail.com>
2023-08-20 15:36:35 -0700

    Add Register Component

---
+++ src/components/User/Register.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./Register.css";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="container">
+      <h2>Account Registration</h2>
+      <div className="input-container">
+        <label>Name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+          required
+        />
+      </div>
+      <div className="input-container">
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div className="input-container">
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+      </div>
+      <button className="button-container">Sign Up</button>
+      <hr />
+      <button className="button-container">Login</button>
+    </div>
+  );
+};
+
+export default Register;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Register from "./components/User/Register";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="register">Register</Link>
+      </div>
+    ),
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
