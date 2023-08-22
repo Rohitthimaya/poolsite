287a1667c744d24015ff6f9ac9ff4e63d6265098
Rohitthimaya <thimayarohit@gmail.com>
2023-08-21 21:07:20 -0700

    Implemented Title section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Home.css';
-import './Navbar/Navbar'
 import Navbar from './Navbar/Navbar';
+import TitlePage from './Title/TitlePage'; 
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <h1>Welcome to My Website</h1>
-      <p>This is the home page of my awesome website.</p>
+      < TitlePage />
     </div>
   );
 }
