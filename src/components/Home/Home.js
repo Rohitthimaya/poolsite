48e8ab305eb470fca7c4c39780d10f07b1355e7a
Rohitthimaya <thimayarohit@gmail.com>
2023-08-21 20:52:49 -0700

    Implemented responsive navbar component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Home.css';
+import './Navbar/Navbar'
+import Navbar from './Navbar/Navbar';
 
 const Home = () => {
   return (
     <div className="home">
+      <Navbar />
       <h1>Welcome to My Website</h1>
       <p>This is the home page of my awesome website.</p>
     </div>
