39b1cd57ac0cdf0b1130bbd66c73b2cc9c333012
Rohitthimaya <thimayarohit@gmail.com>
2023-08-21 21:15:57 -0700

    Implemented Footer Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Home.css';
 import Navbar from './Navbar/Navbar';
 import TitlePage from './Title/TitlePage'; 
+import Footer from './Footer/Footer'
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      < TitlePage />
+      <TitlePage />
+      <Footer />
     </div>
   );
 }
