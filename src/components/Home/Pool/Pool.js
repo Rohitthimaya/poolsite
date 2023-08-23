0d0850e0b87d62a7a9d54d020d76098dd16b6764
Rohitthimaya <thimayarohit@gmail.com>
2023-08-22 21:39:41 -0700

    Implemented Pool Detail Page

--- src/components/Home/Pool/Pool.js
+++ src/components/Home/Pool/Pool.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Pool.css';
+import PoolDetails from '../PoolDetails/PoolDetails';
 
-const Pool = ({ name, ticketPrice, duration }) => {
+const Pool = ({ name, ticketPrice, duration, firstPrize, secondPrize, thirdPrize }) => {
+  const [entered, setEntered] = useState(false);
+  const durationInSeconds = parseDurationToSeconds(duration); // Calculate duration in seconds
+
+  const enterPool = () => {
+    setEntered(true);
+  };
+
+  const closeDetails = () => {
+    setEntered(false);
+  };
+
   return (
     <div className="pool">
       <h2>{name}</h2>
       <p>Ticket Price: {ticketPrice}</p>
       <p>Duration: {duration}</p>
-      <button className="enter-button">Enter Pool</button> {/* Add the button */}
+      {entered ? (
+        <PoolDetails
+          onClose={closeDetails}
+          users={['User 1', 'User 2', 'User 3']} // Replace with actual users
+          firstPrize={firstPrize}
+          secondPrize={secondPrize}
+          thirdPrize={thirdPrize}
+          poolDuration={durationInSeconds}
+        />
+      ) : (
+        <button className="enter-button" onClick={enterPool}>Enter Pool</button>
+      )}
     </div>
   );
 };
 
 export default Pool;
+
+function parseDurationToSeconds(duration) {
+  const weeks = parseInt(duration.split(' ')[0]);
+  return weeks * 7 * 24 * 60 * 60; // Convert weeks to seconds
+}
