e36d35dcda5f207f1e4deb0998d065313a561753
Rohitthimaya <thimayarohit@gmail.com>
2023-08-24 20:59:07 -0700

    Reverted Back to frontent

--- backend/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import CustomUserSerializer
-from .models import CustomUser
-
-
-class CustomUserViewSet(viewsets.ModelViewSet):
-    serializer_class = CustomUserSerializer
-    queryset = CustomUser.objects.all()
