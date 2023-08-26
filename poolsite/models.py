a60e3bd42314b1d64935fd2fdf6460ad084134af
A-dith-ya <awedithya@gmail.com>
2023-08-25 23:16:37 -0700

    Add pool, participant, winner models

--- poolsite/models.py
+++ poolsite/models.py
@@ -2,9 +2,46 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15, blank=True)
     points = models.IntegerField(default=0)
 
     def __str__(self):
         return self.username
+
+class Pool(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('finished', 'Finished'),
+    ]
+
+    pool_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    ticket_cost = models.DecimalField(max_digits=10),
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='active')
+
+    def __str__(self):
+        return self.name
+
+class Participant(models.Model):
+    participant_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    pool = models.ForeignKey(Pool, on_delete=models.CASCADE)
+    entering_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Participant {self.participant_id}"
+
+class Winners(models.Model):
+    winner_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    pool = models.ForeignKey(Pool, on_delete=models.CASCADE)
+    winning_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Winner {self.winner_id}"
